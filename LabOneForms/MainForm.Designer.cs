d3e00f7bd31c6ee2a4e702fdd6e6f951c74f0bf1
HungryDoodles <HungryDoodles@yandex.ru>
2016-10-22 08:32:17 +0500

    Initial push (waving progress bar)

--- LabOneForms/MainForm.Designer.cs
+++ LabOneForms/MainForm.Designer.cs
@@ -29,11 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.wavingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.heartbeatTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // wavingProgressBar
+            // 
+            this.wavingProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.wavingProgressBar.Location = new System.Drawing.Point(13, 13);
+            this.wavingProgressBar.Name = "wavingProgressBar";
+            this.wavingProgressBar.Size = new System.Drawing.Size(259, 23);
+            this.wavingProgressBar.TabIndex = 0;
+            // 
+            // heartbeatTimer
+            // 
+            this.heartbeatTimer.Enabled = true;
+            this.heartbeatTimer.Interval = 16;
+            this.heartbeatTimer.Tick += new System.EventHandler(this.heartbeatTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.wavingProgressBar);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar wavingProgressBar;
+        private System.Windows.Forms.Timer heartbeatTimer;
     }
 }
 
--- LabOneForms/MainForm.cs
+++ LabOneForms/MainForm.cs
@@ -12,9 +12,21 @@ namespace LabOneForms
 {
     public partial class MainForm : Form
     {
+        double time = 0;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void heartbeatTimer_Tick(object sender, EventArgs e)
+        {
+            time += 0.03;
+            wavingProgressBar.Value = (int)(((Math.Sin(time) + 1) * 0.5) * 100);
         }
     }
 }
